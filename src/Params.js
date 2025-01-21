2bb513814c105122608cae6da7c752c16322d0f7
o.podilko <opodilko122@gmail.com>
2025-01-21 13:40:08 +0200

    fix notification and change target date

--- src/Params.js
+++ src/Params.js
@@ -2,6 +2,6 @@ export default class Params {
   static keyPush = 'dfbb9eec-77f6-4316-9129-6e926a3a9228'; // ключ OneSignal
   static appID = '6740279561'; // ID додатку в сторі
   static keyApps = 'BX5RmT4UeiWBs7XFeqzzDM'; // ключ Appsflyer
-  static targetDate = '2025-01-23'; // дата закінчення відльожки
+  static targetDate = '2025-01-24'; // дата закінчення відльожки
   static bodyLin = 'https://astounding-regal-euphoria.space/WThNBGGG'; // тіло лінки з якою будемо працювати
 }
